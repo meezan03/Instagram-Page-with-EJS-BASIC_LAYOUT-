196c5105b289280ce58d8d35afef40bb5d6604aa
Meezan Faisal <meezanfaisal786@gmail.com>
2026-03-25 14:08:14 +0530

    add file

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+let port = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res) => {
+  // res.send("this is home");
+  res.render("home");
+});
+
+// app.get("/ig/:username", (req, res) => {
+//   const followers = ["adam", "bob", "steve", "abc"];
+//   let {username} = req.params;
+//   res.render("instagram.ejs", {username, followers});
+// });
+
+app.get("/ig/:username", (req, res) => {
+  let {username} = req.params;
+  const instaData = require("./data.json");
+  const data = instaData[username];
+  if(data){
+    res.render("instagram.ejs", { data });
+  }else{
+    res.render("error.ejs");
+  }
+  
+});
+
+app.get("/hello", (req, res) => {
+  res.send("hello");
+});
+
+app.get("/rolldice", (req, res) => {
+  let diceVal = Math.floor(Math.random() * 6) + 1;
+  res.render("rollDice.ejs", {diceVal});
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
